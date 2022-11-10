f414bf11cdffce573844e37d5f694fb8c6e10c67
Fabio Mendes <fabiojmendes@gmail.com>
2022-11-10 21:57:26 +0000

    Adding bootstrap script

---
+++ .local/share/cfg/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+
+sudo=''
+if [ $(id -u) -ne 0 ]; then
+┊ sudo='sudo'
+fi
+
+echo "Installing shell goodies"
+if [ -f /etc/redhat-release ]; then
+┊ $sudo dnf install -y vim git-core fish fzf tmux curl sqlite
+elif [ -f /etc/debian_version ]; then
+┊ $sudo apt update && $sudo apt install -y git fish fzf tmux vim curl
+fi
+
+git clone --bare --recursive https://github.com/fabiojmendes/cfg.git $HOME/.local/share/cfg
+
+alias cfg='git --git-dir=$HOME/.local/share/cfg/ --work-tree=$HOME'
+
+cfg submodule update --init
+cfg checkout
