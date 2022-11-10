2d1913bb9870a8b3d5bc611831d433551a5d471c
Fabio Mendes <fabiojmendes@gmail.com>
2022-11-10 22:48:10 +0000

    Fixing colors

--- .config/fish/conf.d/theme.fish
+++ .config/fish/conf.d/theme.fish
@@ -5,7 +5,9 @@ set -U fish_color_quote yellow
 set -U fish_color_redirection cyan --bold
 set -U fish_color_end green
 set -U fish_color_error brred
+set -U fish_color_keyword yellow
 set -U fish_color_param normal
+set -U fish_color_option purple
 set -U fish_color_comment 767676
 set -U fish_color_selection white --bold --background=brblack
 set -U fish_color_search_match --background=555
@@ -24,7 +26,6 @@ set -U fish_pager_color_progress brwhite --background=cyan
 set -U fish_pager_color_completion normal
 set -U fish_pager_color_description B3A06D --italics
 set -U fish_pager_color_selected_background --reverse
-set -U fish_color_keyword
 set -U fish_pager_color_secondary_prefix
 set -U fish_pager_color_secondary_background
 set -U fish_pager_color_selected_description
@@ -34,4 +35,3 @@ set -U fish_color_host_remote
 set -U fish_pager_color_selected_prefix
 set -U fish_pager_color_secondary_description
 set -U fish_pager_color_selected_completion
-set -U fish_color_option
