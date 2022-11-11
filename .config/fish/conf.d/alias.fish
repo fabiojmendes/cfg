ac9065ce2b6155938226ba5d6f2437179e268079
Fabio Mendes <fabiojmendes@gmail.com>
2022-11-11 22:23:50 +0000

    Fix bootstrap

--- .config/fish/conf.d/alias.fish
+++
@@ -1,1 +0,0 @@
-alias cfg='git --git-dir=$HOME/.local/share/cfg --work-tree=$HOME'
--- .config/fish/conf.d/bootstrap.fish
+++ .config/fish/conf.d/bootstrap.fish
@@ -1,5 +1,11 @@
-if not functions -q fisher
-  # Install fisher
-  curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-  fisher install PatrickF1/fzf.fish
+# if not functions -q fisher
+#   # Install fisher
+#   curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+#   fisher install PatrickF1/fzf.fish
+# end
+if set -q __cfg_bootstraped
+  return
 end
+
+echo "Executing bootstrap"
+set -U __cfg_bootstraped
