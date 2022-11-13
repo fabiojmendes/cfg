7da5789da26c21be4336cc4fb7da88c116ca444b
Fabio Mendes <fabiojmendes@gmail.com>
2022-11-12 23:22:02 -0500

    Configure git prompt

--- .local/share/cfg/first_login.fish
+++ .local/share/cfg/first_login.fish
@@ -4,3 +4,9 @@ echo "Executing first login configs"
 
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install PatrickF1/fzf.fish
+
+# Configure git prompt
+set -U __fish_git_prompt_showcolorhints
+set -U __fish_git_prompt_showdirtystate
+set -U __fish_git_prompt_showuntrackedfiles
+set -U __fish_git_prompt_show_informative_status
