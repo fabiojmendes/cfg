44813db44130f88ce5a636a4a5d27e1a53d34292
Fabio Mendes <fabiojmendes@gmail.com>
2022-11-17 23:46:40 -0500

    Formatting cfg function and updating submodules

--- .config/fish/functions/cfg.fish
+++ .config/fish/functions/cfg.fish
@@ -1,10 +1,10 @@
 function cfg --wraps='git --git-dir=$HOME/.local/share/cfg/.git --work-tree=$HOME' --description 'Cfg utils'
     set -l git_args --git-dir=$HOME/.local/share/cfg/.git --work-tree=$HOME
-    if [ $argv[1] = 'update' ]
+    if [ $argv[1] = update ]
         echo 'Performing cfg update'
         git $git_args pull
         git $git_args submodule update --remote --merge
     else
         git $git_args $argv
     end
-end 
+end
