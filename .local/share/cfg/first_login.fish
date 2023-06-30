1faf923e5661a887955602b4e1918b0f2687a63d
Fabio Mendes <fabiojmendes@gmail.com>
2023-06-29 23:20:03 -0400

    Adding fish git config

--- .local/share/cfg/first_login.fish
+++ .local/share/cfg/first_login.fish
@@ -6,7 +6,7 @@ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install PatrickF1/fzf.fish
 
 # Configure git prompt
-set -U __fish_git_prompt_showcolorhints
-set -U __fish_git_prompt_showdirtystate
-set -U __fish_git_prompt_showuntrackedfiles
-set -U __fish_git_prompt_show_informative_status
+set -U __fish_git_prompt_showcolorhints 1
+set -U __fish_git_prompt_showdirtystate 1
+set -U __fish_git_prompt_showuntrackedfiles 1
+set -U __fish_git_prompt_show_informative_status 1
