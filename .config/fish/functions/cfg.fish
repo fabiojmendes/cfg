302f6b06e4802eb8316443e342601d3d21417dab
Fabio Mendes <fabiojmendes@gmail.com>
2025-05-31 14:33:31 -0400

    Update submodule behavior

--- .config/fish/functions/cfg.fish
+++ .config/fish/functions/cfg.fish
@@ -3,7 +3,7 @@ function cfg --wraps='git --git-dir=$HOME/.local/share/cfg/.git --work-tree=$HOM
     if [ $argv[1] = update ]
         echo 'Performing cfg update'
         git $git_args pull
-        git $git_args submodule update --remote --merge
+        git $git_args submodule update --remote
         fisher update
     else
         git $git_args $argv
