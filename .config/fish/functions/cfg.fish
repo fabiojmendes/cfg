a88f1a6a71842720496a8925e61ecd25c1614ced
Fabio Mendes <fabiojmendes@gmail.com>
2025-05-31 16:32:12 -0400

    Update cfg function

--- .config/fish/functions/cfg.fish
+++ .config/fish/functions/cfg.fish
@@ -5,6 +5,11 @@ function cfg --wraps='git --git-dir=$HOME/.local/share/cfg/.git --work-tree=$HOM
         git $git_args pull
         git $git_args submodule update --remote
         fisher update
+    else if [ $argv[1] = fix-detached ]
+        echo 'Performing cfg update'
+        git $git_args pull
+        git $git_args submodule foreach 'git switch master || git switch main'
+        git $git_args submodule update --remote
     else
         git $git_args $argv
     end
