7596374ea66db0c7aee77ee3eb8f7f4187c039eb
Fabio Mendes <fabiojmendes@gmail.com>
2025-06-02 11:30:41 -0400

    Change cfg update behavior

--- .config/fish/functions/cfg.fish
+++ .config/fish/functions/cfg.fish
@@ -2,14 +2,12 @@ function cfg --wraps='git --git-dir=$HOME/.local/share/cfg/.git --work-tree=$HOM
     set -l git_args --git-dir=$HOME/.local/share/cfg/.git --work-tree=$HOME
     if [ $argv[1] = update ]
         echo 'Performing cfg update'
-        git $git_args pull
-        git $git_args submodule update --remote
+        git $git_args pull --recurse-submodules
         fisher update
     else if [ $argv[1] = fix-detached ]
-        echo 'Performing cfg update'
-        git $git_args pull
+        echo 'Fixing detached submodules'
         git $git_args submodule foreach 'git switch master || git switch main'
-        git $git_args submodule update --remote
+        git $git_args pull --recurse-submodules
     else
         git $git_args $argv
     end
