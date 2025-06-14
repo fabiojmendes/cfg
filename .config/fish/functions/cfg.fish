f49be5923e2ac60164c404f20581f71c0184ff02
Fabio Mendes <fabiojmendes@gmail.com>
2025-06-13 21:25:21 -0400

    Refactor cfg installation

--- .config/fish/functions/cfg.fish
+++ .config/fish/functions/cfg.fish
@@ -6,14 +6,13 @@ function cfg --wraps='git --git-dir=$HOME/.local/share/cfg/.git --work-tree=$HOM
         case install
             git $git_args config --local status.showUntrackedFiles no
             git $git_args submodule update --init --remote
-            _cfg_configure_prompt
-            _cfg_install_fisher
             _cfg_vim_tags
+            _cfg_install
         case update
             echo 'Performing cfg update'
             git $git_args pull --recurse-submodules
-            fisher update
             _cfg_vim_tags
+            fisher update
         case fix-detached
             echo 'Fixing detached submodules'
             git $git_args submodule sync --recursive
@@ -25,18 +24,20 @@ function cfg --wraps='git --git-dir=$HOME/.local/share/cfg/.git --work-tree=$HOM
     end
 end
 
-function _cfg_configure_prompt
+function _cfg_install
+    if functions -q fisher
+        fisher update
+    else
+        echo "Installing fisher"
+        curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+        fisher install PatrickF1/fzf.fish
+    end
+
     # Configure git prompt
     set -U __fish_git_prompt_showcolorhints 1
     set -U __fish_git_prompt_showdirtystate 1
     set -U __fish_git_prompt_showuntrackedfiles 1
     set -U __fish_git_prompt_show_informative_status 1
-end
-
-function _cfg_install_fisher
-    echo "Installing fisher"
-    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-    fisher install PatrickF1/fzf.fish
 end
 
 function _cfg_vim_tags
