1dcb1d0def182a3eb3c0483235d0635ed6bf5ec5
Fabio Mendes <fabiojmendes@gmail.com>
2025-06-14 14:45:24 -0400

    Ownership warning

--- .config/fish/conf.d/ownership.fish
+++ .config/fish/conf.d/ownership.fish
@@ -1,8 +1,10 @@
-set -l FILES $(find $HOME -uid 0 | wc -l)
+if status is-interactive && test $(uname) != Darwin
+    set -l FILES $(fd $HOME -uid 0 | wc -l)
 
-if test $FILES -ne 0
-  echo -e '\n'
-  echo "## Found $FILES files with root ownership in your home directory"
-  echo "## Use sudo chown $USER:$USER -R $HOME to fix it"
-  echo -e '\n'
+    if test $FILES -ne 0
+        echo -e '\n'
+        echo "## Found $FILES files with root ownership in your home directory"
+        echo "## Use sudo chown $USER:$USER -R $HOME to fix it"
+        echo -e '\n'
+    end
 end
